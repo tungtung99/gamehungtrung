5cd9e3b25ed66a8a32c8f51b80c45b5643629c05
tungtung99 <tungnguyen99hn1@gmail.com>
2020-12-11 20:06:10 +0700

    update

--- basket.js
+++ basket.js
@@ -1,15 +1,19 @@
 function basket(x, y) {
     this.x = x;
     this.y = y;
-    this.vX = 5
+    this.vX = 8
     this.vY = 0
     this.moveLeft = function () {
-        this.x -= this.vX
-        this.y += this.vY
+        if (this.x >= 20) {
+            this.x -= this.vX
+            this.y += this.vY
+        }
     }
     this.moveRight = function () {
-        this.x += this.vX
-        this.y += this.vY
+        if (this.x <= 480) {
+            this.x += this.vX
+            this.y += this.vY
+        }
     }
     this.draw = function (ctx) {
         let img = new Image()
--- egg.js
+++ egg.js
@@ -1,16 +1,17 @@
 function egg(x, y) {
     this.x = x;
     this.y = y;
-    this.vX = 0
-    this.vY = 1
+    this.vX = 0;
+    this.vY = 1;
     this.run = function () {
         this.x += this.vX
         this.y += this.vY
     }
     this.draw = function (ctx) {
-        /*let img = new Image()
-        img.src = "images/egg1.png"*/
-        let img =[new Image(),new Image()]
+        let img = new Image()
+        img.src = "images/egg1.png"
+        ctx.drawImage(img, this.x, this.y);
+        /*let img =[new Image(),new Image()]
         img[0].src="images/egg1.png"
         img[1].src="images/egg2.png"
         let x =Math.round(Math.random() * 2)
@@ -19,7 +20,7 @@ function egg(x, y) {
         }
         else{
             ctx.drawImage(img[1], this.x, this.y);
-        }
+        }*/
     }
     this.drawEggFall = function (ctx) {
         let img = new Image()
